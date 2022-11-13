c5b57a9c4186b57f43b5c142e86b0d6986286008
intrand <intrand@users.noreply.github.com>
2022-11-13 13:29:09 -0600

    update cloud-init for ubuntu 22.04

--- cloudinit.go
+++ cloudinit.go
@@ -85,67 +85,114 @@ func genCloudInitScript(config cloudInit) error {
 		config.Domain = strings.Join(splitFQDN[1:], ".")
 	}
 	templt := "fqdn: {{ .FQDN }}\n" +
+		"growpart:\n" +
+		"  mode: \"off\"\n" +
+		"disable_root: true\n" +
+		"ssh_deletekeys: false\n" +
+		"preserve_hostname: true\n" +
+		"resize_rootfs: false\n" +
+		"packages_update: true\n" +
+		"packages_upgrade: true\n" +
+		"packages:\n" +
+		"  - openssh-server\n" +
 		"write_files:\n" +
-		"- path: /etc/cloud/cloud.cfg.d/99-custom-networking.cfg\n" +
-		"  permissions: '0644'\n" +
-		"  content: |\n" +
-		"    network: {config: disabled}\n" +
-		"- path: /etc/netplan/config.yaml\n" +
-		"  permissions: '0644'\n" +
-		"  content: |\n" +
-		"    ---\n" +
-		"    network:\n" +
-		"      version: 2\n" +
-		"      renderer: networkd\n" +
-		"      ethernets:\n" +
-		"        {{ .NICName }}:\n" +
-		"          optional: yes\n"
+		"  - path: /etc/netplan/00-installer-config.yaml\n" +
+		"    permissions: 0o644\n" +
+		"    content: |\n" +
+		"      ---\n" +
+		"      network: {}\n" +
+		"  - path: /etc/netplan/50-cloud-init.yaml\n" +
+		"    permissions: 0o644\n" +
+		"    content: |\n" +
+		"      ---\n" +
+		"      network: {}\n" +
+		"  - path: /etc/hosts\n" +
+		"    permissions: 0o644\n" +
+		"    content: |\n" +
+		"      127.0.0.1 localhost\n" +
+		"      127.0.1.1 {{ .FQDN }} {{ .Hostname }}\n" +
+		"      \n" +
+		"      # The following lines are desirable for IPv6 capable hosts\n" +
+		"      ::1     ip6-localhost ip6-loopback\n" +
+		"      fe00::0 ip6-localnet\n" +
+		"      ff00::0 ip6-mcastprefix\n" +
+		"      ff02::1 ip6-allnodes\n" +
+		"      ff02::2 ip6-allrouters\n" +
+		"  - path: /etc/cloud/cloud.cfg.d/no_datasource.cfg\n" +
+		"    permissions: 0o644\n" +
+		"    content: |\n" +
+		"      datasource:\n" +
+		"        None: {}\n" +
+		"      datasource_list:\n" +
+		"        - None\n" +
+		"  - path: /etc/cloud/cloud.cfg.d/99-custom-networking.cfg\n" +
+		"    permissions: 0o644\n" +
+		"    content: |\n" +
+		"      network: {config: disabled}\n" +
+		"  - path: /etc/netplan/config.yaml\n" +
+		"    permissions: 0o644\n" +
+		"    content: |\n" +
+		"      ---\n" +
+		"      network:\n" +
+		"        version: 2\n" +
+		"        renderer: networkd\n" +
+		"        ethernets:\n" +
+		"          {{ .NICName }}:\n" +
+		"            optional: yes\n"
 	if len(config.CIDR) > 0 {
 		templt = templt +
-			"          dhcp4: no\n" +
-			"          dhcp6: no\n" +
-			"          addresses:\n" +
-			"            - {{ .CIDR }}\n" +
-			"          gateway4: {{ .Gateway4 }}\n"
+			"            dhcp4: no\n" +
+			"            dhcp6: no\n" +
+			"            addresses:\n" +
+			"              - {{ .CIDR }}\n" +
+			"            routes:\n" +
+			"              - to: default\n" +
+			"                via: {{ .Gateway4 }}\n"
 		if len(config.DNSAddresses) > 0 {
 			templt = templt +
-				"          nameservers:\n" +
-				"            addresses:\n"
+				"            nameservers:\n" +
+				"              addresses:\n"
 			for _, addr := range config.DNSAddresses {
 				templt = templt +
-					"              - " + addr + "\n"
+					"                - " + addr + "\n"
 			}
 		}
 	} else {
 		templt = templt +
-			"          dhcp4: yes\n" +
-			"          dhcp6: no\n"
+			"            dhcp4: yes\n" +
+			"            dhcp6: no\n"
 	}
 	templt = templt +
 		"runcmd:\n" +
-		"- sed -i 's/template.domain template/{{ .FQDN }} {{ .Hostname }}/g' /etc/hosts  \n" +
-		"- date > /opt/.creation\n" +
-		"- rm /etc/netplan/50-cloud-init.yaml\n" +
-		"- netplan generate\n" +
-		"- netplan apply\n" +
-		"- dpkg-reconfigure openssh-server\n" +
+		"  - date > /opt/.creation\n" +
+		"  - netplan apply\n" +
 		"users:\n" +
-		"- default\n" +
-		"- name: {{ .User }}\n"
+		"  - name: {{ .User }}\n"
 	if len(config.SSHPubKey) > 0 {
 		templt = templt +
-			"  ssh_authorized_keys:\n" +
-			"  - \"{{ .SSHPubKey }}\"\n"
+			"    ssh_authorized_keys:\n" +
+			"      - \"{{ .SSHPubKey }}\"\n"
 	}
 
 	conf, err := template.New("conf").Parse(templt)
 	if err != nil {
 		return err
 	}
-	err = conf.Execute(os.Stdout, config)
+
+	out := os.Stdout
+	if len(*cmd_cloud_init_create_output) > 0 {
+		out, err = os.Create(*cmd_cloud_init_create_output)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = conf.Execute(out, config)
 	if err != nil {
 		return err
 	}
+
+	out.Close()
 
 	return nil
 }
